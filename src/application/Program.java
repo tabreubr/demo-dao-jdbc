1523f1fc4eb8814970371e14df40659a9cd9105b
tabreubr <tabreu.br@gmail.com>
2025-01-27 20:00:43 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -25,10 +26,15 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("\n===== TEST 2: seller findAll =====");
+        System.out.println("\n===== TEST 3: seller findAll =====");
         list = sellerDao.findAll();
         for(Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("\n===== TEST 3: seller findAll =====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
